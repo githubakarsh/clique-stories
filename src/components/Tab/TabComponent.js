a549d1f656ee985ea39bfabcae33a9ac50d8b2a8
Akarsh Saraff <saraffakarsh@gmail.com>
2021-04-01 19:42:18 +0100

    basic tab component

--- src/components/Tab/TabComponent.js
+++ src/components/Tab/TabComponent.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
+
 import './tab.scss';
 
 const tab1 = <div>Tab 1 Content</div>;
@@ -9,30 +11,44 @@ const tabsList = [
     'Tab 1', 'Tab 2', 'Tab 3'
 ]
 const TabComponent = ({
-    namer
+    namer,
+    tabNames
 }) => {
     const [activeTab, setActiveTab] = useState(0);    
-
+    
     const tabComponent = (id) => ({
         0: tab1,
         1: tab2,
         2: tab3
     })[id];
-
+    
   return (
-    <div>
-      <div>
+      
+    <div className="tab-main">
+      <div className="tabbers-main">
+        <nav>
         <ul>
-          {tabsList.map((item, index) => {
+          {tabNames.map((item, index) => {
               return <li onClick={() => setActiveTab(index)}>{item}</li>
           })}
         </ul>
+        </nav>
       </div>
-      <div>
+      <div className="tab-content-container">
            {tabComponent(activeTab)}
       </div>
     </div>
   );
 };
+
+TabComponent.propTypes = {
+
+}
+
+TabComponent.defaultProps = {
+    tabNames: tabsList,
+}
+
+
 
 export default TabComponent;
